f6b31d5e114af4122902d063cd99924040eca50f
dabin2011 <dabin201100@gmail.com>
2025-10-06 21:48:35 +0900

    Update main.js

--- public/main.js
+++ public/main.js
@@ -23,7 +23,7 @@ let ads = [];
 let adIndex = 0;
 let alreadyPlayed = false;
 
-const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbwH3tQUdLyeyrdDNGLXLtmDDIZqUXK-axeSFKLgQPrFLKWEczxz3xLuXcYZ5wNatg9d/exec";
+const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbyVW8CTlKHQves-srvHlHZ0xEb4jyknCoF-lwLVLlPmJ1JhxepscyxXE9v4fsyNNY4W/exec";
 
 // 音声切り替え
 enableBtn.addEventListener('click', () => {
@@ -156,6 +156,7 @@ function triggerJihou(video, audio) {
 }
 </body>
 </html>
+
 
 
 

2d3e83865d9126c07f670b5ab6c2ba56bcd2a782
dabin2011 <dabin201100@gmail.com>
2026-04-14 20:03:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
-let enabled = true;
-const video = document.getElementById("jingleVideo");
-const overlay = document.getElementById("overlay");
-const soundToggle = document.getElementById("soundToggle");
+window.onload = () => {
+  const overlay = document.getElementById("overlay");
+  const video = document.getElementById("jingleVideo");
 
-soundToggle.addEventListener("click", () => {
-  enabled = !enabled;
-  soundToggle.textContent = enabled ? "🔊" : "🔇";
-});
-
-setInterval(checkTime, 1000);
-
-function checkTime() {
-  if (!enabled) return;
-
-  const now = new Date();
-  const h = now.getHours();
-  const m = now.getMinutes();
-  const s = now.getSeconds();
-
-  if (h === 23 && m === 59 && s === 0) {
-    playVideo();
-  }
-}
-
-function playVideo() {
   overlay.classList.add("show");
   video.currentTime = 0;
   video.play();
@@ -31,4 +9,4 @@ function playVideo() {
   video.onended = () => {
     overlay.classList.remove("show");
   };
-}
+};
